f0c741ef5f93f8b9c5c8fb5e3ba16c96f94e19a3
Miles Loveday <miles.loveday@and.digital>
2025-07-02 12:13:56 +0100

    Remove loading spinner from error component

--- packages/client/src/components/common/api/api-error.tsx
+++ packages/client/src/components/common/api/api-error.tsx
@@ -1,9 +1,6 @@
-import { ApiLoading } from "@repo/client/components/common/api/api-loading.tsx";
-
-export const ApiError = ({ retry }: { retry: () => void }) => (
+export const ApiError = ({retry}: { retry: () => void }) => (
   <div className="flex flex-col gap-2">
     <span>There was an error loading this content.</span>
-    <ApiLoading />
     <button type="button" onClick={retry}>
       Retry
     </button>
